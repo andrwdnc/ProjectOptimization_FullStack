c6084d108362907a2056646fb3996235358dbbc4
jormel <jorgeferrandizmellado@gmail.com>
2022-12-27 21:43:52 +0100

    finalizando crud productos

--- backEnd/controllers/product.controller.js
+++ backEnd/controllers/product.controller.js
@@ -10,3 +10,68 @@ exports.findAll = async (req, res) => {
         return res.status(500).json({ error: "error de servidor" });
     }
 };
+
+exports.findId = async(req,res)=>{
+    try{
+        const { id } = req.params
+        const product = await Product.findById(id)
+        if(product){
+            return res.json({product})
+        }else{
+            console.log("Producto no encontrado")
+        }
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({error: "error de servidor"})
+    }
+}
+
+exports.create = async(req,res)=>{
+    try{
+        
+        const newProduct = new Product(req.body)
+        const resultado = await newProduct.save()
+        if(resultado){
+            return res.json({resultado})
+        }else{
+            console.log("Error creando producto")
+        }
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({error: "error de servidor"})
+    }
+}
+
+exports.update = async(req,res)=>{
+    try{
+        const {id} = req.params
+        const pActualizado = await Product.findByIdAndUpdate(id,req.body,{ runValidators:true, new:true})
+        if(pActualizado){
+            return res.json({pActualizado})
+        }else{
+            console.log("Error actualizando")
+        }
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({error: "error de servidor"})
+    }
+}
+
+
+
+
+exports.delete = async(req,res)=>{
+    try{
+        const { id } = req.params
+        const resultado = await Product.findByIdAndDelete(id)
+        if(resultado){
+            return res.json({resultado})
+        }else{
+            console.log("El producto no se ha podido eliminar")
+        }
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({error: "error de servidor"})
+    }
+}
+
--- backEnd/routes/product.routes.js
+++ backEnd/routes/product.routes.js
@@ -3,5 +3,9 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/",productController.findAll)
+router.get("/:id",productController.findId)
+router.post("/",productController.create)
+router.patch("/:id",productController.update)
+router.delete("/:id",productController.delete)
 
 module.exports = router
